c354208686f0519a0564c0ed07302ed415d9d061
Jake Hammond <jakehammond226@gmail.com>
2022-07-05 10:47:52 -0500

    method2

---
+++ methodsexercise2/Methods.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace methodsexercise2
+{
+    public class Methods
+    {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+
+        
+        public static int Multiply(int x, int y)
+        {
+            return x * y; 
+        }
+
+        public static int Divide(int x, int y)
+        {
+            return x / y; 
+        }     
+    }
+}
+
---
+++ methodsexercise2/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using methodsexercise2;
+
+var result1 = Methods.Add(3,6);
+
+Console.WriteLine(result1); 
+
+var result2 = Methods.Subtract(9, 5);
+
+Console.WriteLine(result2);
+
+var result3 = Methods.Multiply(4, 7);
+
+Console.WriteLine(result3);
+
+var result4 = Methods.Divide(9, 3);
+
+Console.WriteLine(result4);
+
+
+
+
